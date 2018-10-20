071818d848944ebd91ba79d576dbc3b586ee81c0
keithatan <tan230@purdue.edu>
2018-10-19 23:26:23 -0400

    Created grab location activity

---
+++ app/src/main/java/com/just/pickaplace/GrabLocationActivity.java
@@ -0,0 +1,25 @@
+package com.just.pickaplace;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GrabLocationActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grab_location);
+
+
+
+    }
+
+
+}
--- app/src/main/java/com/just/pickaplace/MainActivity.java
+++ app/src/main/java/com/just/pickaplace/MainActivity.java
@@ -1,13 +1,38 @@
 package com.just.pickaplace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button startButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_grab_location);
+        startButton = findViewById(R.id.start);
+        startButton.setOnClickListener(listener);
     }
+
+    Button.OnClickListener listener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, GrabLocationActivity.class);
+            /*
+            Bundle extras = new Bundle();
+            extras.putString("Question", edit1.getText().toString());
+            extras.putString("Question2",edit2.getText().toString());
+            extras.putString("Answer", textView.getText().toString());
+            extras.putString("Answer2",textView2.getText().toString());
+
+            intent.putExtras(extras);
+            */
+            startActivity(intent);
+        }
+    };
 }
