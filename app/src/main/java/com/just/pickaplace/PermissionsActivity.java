07d4a30e85fed26f97c9c1c03312705fc0cd4733
Benjamin Earnest <42708925+heav10n@users.noreply.github.com>
2018-10-20 17:59:25 -0400

    Created path from permissions activity to grablocation and a skip statement in case the app already has permissions

--- app/src/main/java/com/just/pickaplace/PermissionsActivity.java
+++ app/src/main/java/com/just/pickaplace/PermissionsActivity.java
@@ -1,6 +1,7 @@
 package com.just.pickaplace;
 
 import android.Manifest;
+import android.content.Intent;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,9 @@ public class PermissionsActivity extends AppCompatActivity {
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, callbackResult);
 
+
+        Intent locationGrab = new Intent(PermissionsActivity.this, GrabLocationActivity.class);
+        startActivity(locationGrab);
 
     }
 }
