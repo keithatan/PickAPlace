86f4d1ecf48e01850d8194065519acf5ff76c091
keithatan <tan230@purdue.edu>
2018-10-19 23:32:25 -0400

    Added foundation for grabbing lcoation

--- app/src/main/java/com/just/pickaplace/GrabLocationActivity.java
+++ app/src/main/java/com/just/pickaplace/GrabLocationActivity.java
@@ -9,17 +9,33 @@ import android.widget.TextView;
 
 public class GrabLocationActivity extends AppCompatActivity {
 
-
-
+    String longitude;
+    String latitude;
+    Button nextButton;
+    String location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grab_location);
-
-
-
+        nextButton = findViewById(R.id.button);
+        nextButton.setOnClickListener(listener);
+        location = "New York City, NYC, 350 5th Ave, New York, NY 10118";
     }
+
+    Button.OnClickListener listener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent( GrabLocationActivity.this, GetWalkingDistance.class);
+
+            Bundle extras = new Bundle();
+            extras.putString("latitude", latitude);
+            extras.putString("longitude", longitude);
+            extras.putString("location", location);
+            intent.putExtras(extras);
+            startActivity(intent);
+        }
+    };
 
 
 }
--- app/src/main/java/com/just/pickaplace/MainActivity.java
+++ app/src/main/java/com/just/pickaplace/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grab_location);
-        startButton = findViewById(R.id.start);
+        startButton = findViewById(R.id.button);
         startButton.setOnClickListener(listener);
     }
 
