0ca8fa0ad0895cb1b1393ca647a5c72a3f14c0a7
Sagar Garg <sagargarg@gmail.com>
2018-12-09 23:50:41 -0500

    Revert "Open now"
    This reverts commit 07c7b3722b21c2c2de2afc2b512f8eb05b413ad9.

--- app/src/main/java/com/just/pickaplace/GeneratePlacesActivity.java
+++ app/src/main/java/com/just/pickaplace/GeneratePlacesActivity.java
@@ -63,7 +63,6 @@ public class GeneratePlacesActivity extends AppCompatActivity {
         String rad = globalInformation.getString("radius");
         String cuisines = globalInformation.getString("cuisines");
         String bud = globalInformation.getString("budget");
-        String open = globalInformation.getString("open");
 
         Random rand = new Random();
         int n = rand.nextInt(4) + 1;
